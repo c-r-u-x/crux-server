66f01497ad5464a99da2fadece08ccec612998b5
Feedbaek <nicek789@naver.com>
2024-06-06 15:12:02 +0900

    Spring Initialize

---
+++ src/main/java/crux/crux_server/CruxServerApplication.java
@@ -0,0 +1,13 @@
+package crux.crux_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruxServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruxServerApplication.class, args);
+	}
+
+}
