c04a07bc7d15b9a0da9d872c9012102b60227966
Feedbaek <nicek789@naver.com>
2024-06-30 17:07:58 +0900

    Feat: graphql 적용해서 member 조회 구현

--- src/main/java/crux/crux_server/domain/member/repository/RoleRepository.java
+++ src/main/java/crux/crux_server/domain/member/repository/RoleRepository.java
@@ -1,6 +1,7 @@
 package crux.crux_server.domain.member.repository;
 
 import crux.crux_server.domain.member.entity.Role;
+import crux.crux_server.domain.member.enums.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    boolean existsByName(String roleName);
+    boolean existsByName(RoleName name);
 
-    Optional<Role> findByName(String roleName);
+    Optional<Role> findByName(RoleName name);
 }
